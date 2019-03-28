fd7afcf60d96af2ad9a95c818390275a92c42e83
Ganapathypinfo <Ganapathypinfo@gmail.com>
2019-03-28 16:48:22 +0530

    #EditProfile image cropping implementation completed #alingment changes completed

--- app/src/main/java/com/club/bhimclub/bhimclub/activity/EditProfileActivity.java
+++ app/src/main/java/com/club/bhimclub/bhimclub/activity/EditProfileActivity.java
@@ -1,45 +1,57 @@
 package com.club.bhimclub.bhimclub.activity;
 
 import android.Manifest;
+import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.provider.Settings;
+import android.support.annotation.Nullable;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.PopupMenu;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AutoCompleteTextView;
 import android.widget.EditText;
 import android.widget.ImageView;
 
+import com.club.bhimclub.bhimclub.GlideApp;
 import com.club.bhimclub.bhimclub.R;
 import com.club.bhimclub.bhimclub.viewModel.ProfileViewModel;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.MultiplePermissionsReport;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener;
 
 import java.io.File;
+import java.io.IOException;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import butterknife.Unbinder;
 
-public class EditProfileActivity extends AppCompatActivity {
+public class EditProfileActivity extends BaseActivity {
 
+    private static final String TAG = EditProfileActivity.class.getSimpleName();
     private Unbinder unbinder;
     private ProfileViewModel mProfileViewModel;
-    private static final int RESULT_PROFILE_IMAGE = 1;
-    private static final int REQ_CAMERA_IMAGE = 2;
 
-    private static final int PERMISSION_REQUEST_CAMERA = 1;
-    private static final int PERMISSION_REQUEST_WRITE = 2;
-    private static final int PERMISSION_REQUEST_READ = 13;
-    private static final int RESULT_CROP = 5;
+    public static final int REQUEST_IMAGE = 100;
+
     private Uri imageUri;
     // UI references.
 
@@ -58,26 +70,41 @@ public class EditProfileActivity extends AppCompatActivity {
     @BindView(R.id.personDepartment)
     EditText mPersonDepartment;
 
+    @BindView(R.id.theamCamera)
+    ImageView mtheamCamera;
+
+    @BindView(R.id.imgTheam)
+    ImageView mimgTheam;
+
+    @BindView(R.id.cpersonview)
+    ImageView mCpersonview;
+
     @BindView(R.id.personCamera)
     ImageView mPersonCamera;
 
 
+    private boolean mWhichImgae;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_profile);
         unbinder = ButterKnife.bind(this);
-
+        loadProfileDefault();
     }
 
     @OnClick(R.id.theamCamera)
-    public void TheamCamera(){
-
+    public void TheamCamera() {
+        mWhichImgae = false;
+        popUpMenuCall(mtheamCamera);
     }
+
     @OnClick(R.id.personCamera)
-    public void PersonCamera(){
+    public void PersonCamera() {
+        mWhichImgae = true;
         popUpMenuCall(mPersonCamera);
     }
+
 
     private static void setForceShowIcon(PopupMenu popupMenu) {
         try {
@@ -98,7 +125,59 @@ public class EditProfileActivity extends AppCompatActivity {
 //            Logger.e(TAG, e.getMessage());
         }
     }
+
     private void popUpMenuCall(View v) {
+        Dexter.withActivity(this)
+                .withPermissions(Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                .withListener(new MultiplePermissionsListener() {
+                    @Override
+                    public void onPermissionsChecked(MultiplePermissionsReport report) {
+                        if (report.areAllPermissionsGranted()) {
+                            showImagePickerOptions(v);
+                        }
+
+                        if (report.isAnyPermissionPermanentlyDenied()) {
+                            showSettingsDialog();
+                        }
+                    }
+
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(List<PermissionRequest> permissions, PermissionToken token) {
+                        token.continuePermissionRequest();
+
+                    }
+
+                }).check();
+
+    }
+
+    /**
+     * Showing Alert Dialog with Settings option
+     * Navigates user to app settings
+     * NOTE: Keep proper title and message depending on your app
+     */
+    private void showSettingsDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(EditProfileActivity.this);
+        builder.setTitle(getString(R.string.dialog_permission_title));
+        builder.setMessage(getString(R.string.dialog_permission_message));
+        builder.setPositiveButton(getString(R.string.go_to_settings), (dialog, which) -> {
+            dialog.cancel();
+            openSettings();
+        });
+        builder.setNegativeButton(getString(android.R.string.cancel), (dialog, which) -> dialog.cancel());
+        builder.show();
+
+    }
+
+    // navigating user to app settings
+    private void openSettings() {
+        Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+        Uri uri = Uri.fromParts("package", getPackageName(), null);
+        intent.setData(uri);
+        startActivityForResult(intent, 101);
+    }
+
+    private void showImagePickerOptions(View v) {
         PopupMenu popup = new PopupMenu(this, v);
         popup.getMenuInflater().inflate(R.menu.profile_option, popup.getMenu());
         setForceShowIcon(popup);
@@ -112,153 +191,86 @@ public class EditProfileActivity extends AppCompatActivity {
         });
         popup.show();
     }
-    private void performMenu(MenuItem menuItem) {
-        Intent intent;
 
-        if (Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {
-            intent = new Intent(Intent.ACTION_PICK, MediaStore.Video.Media.EXTERNAL_CONTENT_URI);
-        } else {
-            intent = new Intent(Intent.ACTION_PICK, MediaStore.Video.Media.INTERNAL_CONTENT_URI);
-        }
+    private void performMenu(MenuItem menuItem) {
+
         switch (menuItem.getItemId()) {
             //image from gallery
             case R.id.image_item:
-//                Logger.d(TAG, " image_item");
-                if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
-                        == PackageManager.PERMISSION_GRANTED) {
-                    // Permission is already available, start camera preview
-                    /*Snackbar.make(ApplicationController.appactivity.findViewById(android.R.id.content),
-                            "permission is available.",
-                            Snackbar.LENGTH_SHORT).show();*/
-                    intent.setType("image/jpg | image/jpeg");
-                    Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-                    startActivityForResult(galleryIntent, RESULT_PROFILE_IMAGE);
-                } else {
-                    // Permission is missing and must be requested.
-                    requestCameraPermission(PERMISSION_REQUEST_READ);
-                }
+                launchGalleryIntent();
                 break;
             case R.id.camera_item:
-//                Logger.d(TAG, " Camera");
-
-                if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
-                        == PackageManager.PERMISSION_GRANTED) {
-                    // Permission is already available, start camera preview
-                    // Permission is already available, start camera preview
-                    if (ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
-                            == PackageManager.PERMISSION_GRANTED) {
-                      /*  Snackbar.make(ApplicationController.appactivity.findViewById(android.R.id.content),
-                            "Camera permission is available. Starting preview.",
-                            Snackbar.LENGTH_SHORT).show();*/
-                        startCamera();
-                    } else {
-                        requestCameraPermission(PERMISSION_REQUEST_WRITE);
-                    }
-                } else {
-                    // Permission is missing and must be requested.
-                    requestCameraPermission(PERMISSION_REQUEST_CAMERA);
-                }
+                launchCameraIntent();
                 break;
             default:
                 break;
         }
     }
 
-    private void startCamera() {
-        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+    private void launchCameraIntent() {
+        Intent intent = new Intent(EditProfileActivity.this, ImagePickerActivity.class);
+        intent.putExtra(ImagePickerActivity.INTENT_IMAGE_PICKER_OPTION, ImagePickerActivity.REQUEST_IMAGE_CAPTURE);
 
-        File sdcard = Environment.getExternalStorageDirectory();
-        File filePath = new File(sdcard + "/bhimclub/Capature/");
-        if(!filePath.exists()){
-            filePath.mkdirs();
-        }
-        File photo = new File(filePath, System.currentTimeMillis() + ".jpg");
-        intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(photo));
-        imageUri = Uri.fromFile(photo);
-        startActivityForResult(intent, REQ_CAMERA_IMAGE);
+        // setting aspect ratio
+        intent.putExtra(ImagePickerActivity.INTENT_LOCK_ASPECT_RATIO, true);
+        intent.putExtra(ImagePickerActivity.INTENT_ASPECT_RATIO_X, 1); // 16x9, 1x1, 3:4, 3:2
+        intent.putExtra(ImagePickerActivity.INTENT_ASPECT_RATIO_Y, 1);
+
+        // setting maximum bitmap width and height
+        intent.putExtra(ImagePickerActivity.INTENT_SET_BITMAP_MAX_WIDTH_HEIGHT, true);
+        intent.putExtra(ImagePickerActivity.INTENT_BITMAP_MAX_WIDTH, 1000);
+        intent.putExtra(ImagePickerActivity.INTENT_BITMAP_MAX_HEIGHT, 1000);
+
+        startActivityForResult(intent, REQUEST_IMAGE);
     }
 
-    /**0
-     * Req0uests the {@link android.Manifest.permission#CAMERA} permission.
-     * If0 an additional rationale should be displayed, the user has to launch the request from
-     * a0 SnackBar that includes additional information.
-     */
-    private void requestCameraPermission(int permissonValue) {
-        switch (permissonValue) {
-            case PERMISSION_REQUEST_CAMERA:
-                if (ActivityCompat.shouldShowRequestPermissionRationale(this,
-                        Manifest.permission.CAMERA)) {
-                    // Provide an00 additional rationale to the user if the permission was not granted
-                    // and the u0ser would benefit from additional context for the use of the permission.
-                    // Display a SnackBar with a button to request the missing permission.
-                    /*Snackbar.make(ApplicationController.appactivity.findViewById(android.R.id.content), "Camera access is required to display the camera preview.",
-                            Snackbar.LENGTH_LONG).setAction("OK", new View.OnClickListener() {
-                        @Override
-                        public void onClick(View view) {*/
-                            // Request the permission
-                            ActivityCompat.requestPermissions(EditProfileActivity.this,
-                                    new String[]{Manifest.permission.CAMERA},
-                                    PERMISSION_REQUEST_CAMERA);
-                  /*      }
-                    }).show();*/
+    private void launchGalleryIntent() {
+        Intent intent = new Intent(EditProfileActivity.this, ImagePickerActivity.class);
+        intent.putExtra(ImagePickerActivity.INTENT_IMAGE_PICKER_OPTION, ImagePickerActivity.REQUEST_GALLERY_IMAGE);
 
-                } else {
-                    /*Snackbar.make(ApplicationController.appactivity.findViewById(android.R.id.content),
-                            "Camera access Permission is not available.",
-                            Snackbar.LENGTH_SHORT).show();*/
-                    // Request the permission. The result will be received in onRequestPermissionResult().
-                    ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA},
-                            PERMISSION_REQUEST_CAMERA);
+        // setting aspect ratio
+        intent.putExtra(ImagePickerActivity.INTENT_LOCK_ASPECT_RATIO, true);
+        intent.putExtra(ImagePickerActivity.INTENT_ASPECT_RATIO_X, 1); // 16x9, 1x1, 3:4, 3:2
+        intent.putExtra(ImagePickerActivity.INTENT_ASPECT_RATIO_Y, 1);
+        startActivityForResult(intent, REQUEST_IMAGE);
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (requestCode == REQUEST_IMAGE) {
+            if (resultCode == Activity.RESULT_OK) {
+                Uri uri = data.getParcelableExtra("path");
+                try {
+                    // You can update this bitmap to your server
+                    Bitmap bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), uri);
+
+                    // loading profile image from local cache
+                    loadProfile(uri.toString());
+                } catch (IOException e) {
+                    e.printStackTrace();
                 }
-                break;
-            case PERMISSION_REQUEST_WRITE:
-                if (ActivityCompat.shouldShowRequestPermissionRationale(this,
-                        Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
-                    /*Snackbar.make(ApplicationController.appactivity.findViewById(android.R.id.content), "Storage access is required to save images.",
-                            Snackbar.LENGTH_LONG).setAction("OK", new View.OnClickListener() {
-                        @Override
-                        public void onClick(View view) {*/
-                            // Request the permission
-                            ActivityCompat.requestPermissions(EditProfileActivity.this,
-                                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
-                                    PERMISSION_REQUEST_WRITE);
-                      /*  }
-                    }).show();*/
-                } else {
-                    /*Snackbar.make(ApplicationController.appactivity.findViewById(android.R.id.content),
-                            "Storage access Permission is not available.",
-                            Snackbar.LENGTH_SHORT).show();*/
-                    // Request the permission. The result will be received in onRequestPermissionResult().
-                    ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
-                            PERMISSION_REQUEST_WRITE);
-                }
-                break;
-            case PERMISSION_REQUEST_READ:
-                if (ActivityCompat.shouldShowRequestPermissionRationale(this,
-                        Manifest.permission.READ_EXTERNAL_STORAGE)) {
-                    /*Snackbar.make(ApplicationController.appactivity.findViewById(android.R.id.content), "Storage access is required to display images.",
-                            Snackbar.LENGTH_LONG).setAction("OK", new View.OnClickListener() {
-                        @Override
-                        public void onClick(View view) {*/
-                            // Request the permission
-                            ActivityCompat.requestPermissions(EditProfileActivity.this,
-                                    new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
-                                    PERMISSION_REQUEST_READ);
-                      /*  }
-                    }).show();*/
-                } else {
-                    /*Snackbar.make(ApplicationController.appactivity.findViewById(android.R.id.content),
-                            "Permissions is not available.",
-                            Snackbar.LENGTH_SHORT).show();*/
-                    // Request the permission. The result will be received in onRequestPermissionResult().
-                    ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
-                            PERMISSION_REQUEST_READ);
-                }
-                break;
-            default:
-                break;
+            }
         }
+    }
 
+    private void loadProfile(String url) {
+        Log.d(TAG, "Image cache path: " + url);
+        if (mWhichImgae) {
+            GlideApp.with(this).load(url)
+                    .into(mCpersonview);
+            mCpersonview.setColorFilter(ContextCompat.getColor(this, android.R.color.transparent));
+        } else {
+            GlideApp.with(this).load(url)
+                    .into(mimgTheam);
+            mimgTheam.setColorFilter(ContextCompat.getColor(this, android.R.color.transparent));
+        }
+    }
+
+    private void loadProfileDefault() {
+        GlideApp.with(this).load(R.drawable.contact_icon_profile)
+                .into(mCpersonview);
+        mCpersonview.setColorFilter(ContextCompat.getColor(this, R.color.profile_default_tint));
     }
 
 }
